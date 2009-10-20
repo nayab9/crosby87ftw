c83d47204e1968bc9851ef792c925bd2eb41bb36
nayab9@gmail.com <nayab9@gmail.com@451588ce-bc75-11de-b528-055f0b42e881>
2009-10-20 04:09:43 +0000

    started the server with hello world!

---
+++ trunk/src/server.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Bayan
+ *
+ */
+public class server 
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+
+	}
+
+}
