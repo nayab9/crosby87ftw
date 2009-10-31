676bc0e0514b57369c3d0cc47798ef1229897013
april.bugnot <april.bugnot@451588ce-bc75-11de-b528-055f0b42e881>
2009-10-31 16:13:22 +0000

    Implemented Player and Game Try out the game using Test -- can play the game one-on-one on same computer using JOptionPane woot woot

---
+++ src/april/Player.java
@@ -0,0 +1,85 @@
+package april;
+
+import java.net.*;
+
+/**
+ * 
+ */
+
+/**
+ * @author April
+ *
+ */
+public class Player {
+	
+	private String userName;
+	private Socket socket;
+	private int threadId;
+	private boolean isBusy;
+	
+	public Player(String userName, Socket socket, int threadId) {
+		this.userName = userName;
+		this.socket = socket;
+		this.threadId = threadId;
+		this.isBusy = false;		
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param socket the socket to set
+	 */
+	public void setSocket(Socket socket) {
+		this.socket = socket;
+	}
+
+	/**
+	 * @return the socket
+	 */
+	public Socket getSocket() {
+		return socket;
+	}
+
+	/**
+	 * @param threadId the threadId to set
+	 */
+	public void setThreadId(int threadId) {
+		this.threadId = threadId;
+	}
+
+	/**
+	 * @return the threadId
+	 */
+	public int getThreadId() {
+		return threadId;
+	}
+
+	/**
+	 * @param isBusy the isBusy to set
+	 */
+	public void setBusy(boolean isBusy) {
+		this.isBusy = isBusy;
+	}
+
+	/**
+	 * @return the isBusy
+	 */
+	public boolean isBusy() {
+		return isBusy;
+	}
+
+
+
+}
