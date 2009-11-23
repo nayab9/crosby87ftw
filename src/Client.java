3e5620900b174cdbef59bdf334ece05de6b79f09
april.bugnot <april.bugnot@451588ce-bc75-11de-b528-055f0b42e881>
2009-11-23 03:14:46 +0000

    added CROSBY87

--- src/Client.java
+++ src/Client.java
@@ -6,60 +6,60 @@ public class Client implements Runnable
 {
 	private Socket connection;
 	private int ID;
-	
-	public static void main(String[] args) 
+
+	public static void main(String[] args)
 	{
 		//host provided via command line
 		String host = args[0];
-		
+
 		//create socket
 		Socket connection = null;
-		try 
+		try
 		{
 			// port number provided by command line
 			int port = Integer.parseInt(args[1]);
 			connection = new Socket(host, port);
-		} 
+		}
 		catch (NumberFormatException nfe)
 		{
 			System.out.println("Port number must be an integer.");
 			System.exit(0);
 		}
-		catch (IOException e) 
+		catch (IOException e)
 		{
 			System.out.println("Unable to connect to "+host+" via port "+args[1]+".");
 			System.exit(0);
 		}
 
 		System.out.println("Client Initialized . . .");
-	
+
 		Runnable runnable1 = new Client(connection, 0);
 		Thread thread1 = new Thread(runnable1);
 		thread1.start();
-		
+
 		Runnable runnable2 = new Client(connection, 1);
 		Thread thread2 = new Thread(runnable2);
 		thread2.start();
 	}
-	
+
 	Client(Socket s, int i)
 	{
 		this.connection = s;
 		this.ID = i;
 	}
-	
+
 	public void run()
 	{
 		try
-		{		
-			BufferedReader inFromUser = 
-				new BufferedReader(new InputStreamReader(System.in));	
+		{
+			BufferedReader inFromUser =
+				new BufferedReader(new InputStreamReader(System.in));
 			BufferedReader inFromServer = new BufferedReader
 				(new InputStreamReader(connection.getInputStream()));
 			PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
-			
+
 			String fromServer, fromUser;
-	
+
 			//listening thread
 			if (this.ID == 0)
 			{
@@ -79,7 +79,7 @@ public class Client implements Runnable
 			{
 				while (((fromUser = inFromUser.readLine()).compareTo("bye") != 0))
 				{
-	
+
 					if (fromUser.compareTo("help") == 0)
 					{
 						printHelp();
@@ -89,18 +89,18 @@ public class Client implements Runnable
 					{
 						fromUser = "crosby87 " + fromUser;
 						out.println(fromUser);
-					}			
+					}
 				}
-				
+
 				fromUser = "crosby87 " + fromUser;
-				out.println(fromUser);			
+				out.println(fromUser);
 			}
 		}
 		catch (Exception e)
 		{
 			//System.out.println(e);
 		}
-	
+
 	}
 
 	public void printHelp()
@@ -123,23 +123,23 @@ public class Client implements Runnable
 		System.out.println("-----------------------------------------------------------------------------");
 		System.out.println("who2 			- 	displays all currently logged in players.");
 		System.out.println("whoami			- 	displays detailed information about yourself");
-		System.out.println("login NAME PASS		- 	where NAME is your unique login name and PASS is password.");		
+		System.out.println("login NAME PASS		- 	where NAME is your unique login name and PASS is password.");
 		System.out.println("crosby doesnt quit 	- 	win your game instantly (admin only).");
 		System.out.println("whisper NAME MSG 	- 	private message MSG to person NAME.");
 		System.out.println("bcast PSEUDO MSG 	- 	broadcast MSG with name PSEUDO (admin only).");
 		System.out.println("-----------------------------------------------------------------------------");
 	}
-	
+
 	public String clientParse(String msg)
 	{
 		String message = "";
-		if (msg.indexOf("200 OK") > -1)
+		if (msg.indexOf("CROSBY87 200 OK") > -1)
 		{
-			message = msg.replace("200 OK", "");
+			message = msg.replace("CROSBY87 200 OK", "");
 		}
-		else if (msg.indexOf("400 ERROR") > -1)
+		else if (msg.indexOf("CROSBY87 400 ERROR") > -1)
 		{
-			message = msg.replace("400 ERROR", "");
+			message = msg.replace("CROSBY87 400 ERROR", "");
 		}
 		return message;
 	}
